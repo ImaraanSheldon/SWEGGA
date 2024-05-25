3d814e5badc565d861fe7e22e5a112e10417c96e
ImaraanSheldon <imaraansheldonis2302003@gmail.com>
2024-05-25 09:56:44 -0700

    Exhausted

--- script/code.js
+++ script/code.js
@@ -1,14 +1,25 @@
-
-
-let tired = document.getElementById('rah')
-
-tired.addEventListener(('click'), ()=>{
-    let fQuantity = document.getElementById('femaleQuantity');
-    let mQuantity = document.getElementById('maleQuantity');
+function tired(){
+    let fQuantity = document.getElementById('femaleQuantity').value;
     let fPrice = document.getElementById('fPrice').value
-    console.log(fPrice)
-    document.getElementById('tPrice').textContent = fPrice.substring(2) * fQuantity.value;
-    console.log(fPrice.substring(2) * fQuantity.value);
 
+    // console.log(fPrice.substring(2) * fQuantity);
+    document.getElementById('tfPrice').value = parseFloat(fPrice.substring(2) * fQuantity).toFixed(2);
+    let tired = fPrice.substring(2) * fQuantity
+    return tired;
 }
-)
+
+function fr(){
+    let mQuantity = document.getElementById('maleQuantity').value;
+    let mPrice = document.getElementById('mPrice').value
+
+    // console.log(mPrice.substring(2) * mQuantity);
+    document.getElementById('tmPrice').value = parseFloat(mPrice.substring(2) * mQuantity).toFixed(2);
+    let fr = mPrice.substring(2) * mQuantity
+    return fr;
+}
+
+function totalPrice(){
+    // console.log(fr())
+    // console.log(tired())
+    document.getElementById('valueMAIN').value ='R ' + parseFloat(tired() + fr()).toFixed(2);
+}
